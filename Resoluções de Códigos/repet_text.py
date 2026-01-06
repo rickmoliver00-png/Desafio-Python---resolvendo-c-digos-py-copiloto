8e18cf69f60abec57a9fd14d8c7126b43ccc4a4d
rickmoliver00-png <rickmoliver00@gmail.com>
2026-01-06 01:04:05 -0300

    Altera soma para multiplicação no cálculo
    Corrige a operação de soma para multiplicação e atualiza a mensagem.

--- Resoluções de Códigos/repet_text.py
+++ Resoluções de Códigos/repet_text.py
@@ -1,1 +1,11 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+
+# Solicita dois números do usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Operação simples: soma
+soma = num1 * num2
+
+print("A soma dos dois números é:", multiplicação)
